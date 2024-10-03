b2098da2cd4b4cc4f0212fb0af082d2b44b49903
neirinzaralwin <zaralwinneirin@gmail.com>
2024-10-03 15:17:58 +0700

    wish list screen done

--- lib/screens/home/wishlist/wishlist_screen.dart
+++ lib/screens/home/wishlist/wishlist_screen.dart
@@ -1,4 +1,5 @@
 import 'dart:io';
+import 'package:fluentui_system_icons/fluentui_system_icons.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_movie_code_test/constants/app_color.dart';
@@ -13,60 +14,87 @@ class WishlistScreen extends StatefulWidget {
 }
 
 class _WishlistScreenState extends State<WishlistScreen> {
+  final bloc = WishlistBloc();
+
+  @override
+  void initState() {
+    bloc.add(GetWishListEvent());
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
           title: Text('Wishlist'),
         ),
-        body: BlocBuilder<WishlistBloc, WishlistState>(builder: (context, state) {
-          if (state is WishlistLoading) {
-            return const Center(child: CircularProgressIndicator.adaptive());
-          }
-          if (state is WishlistError) {
-            return Center(
-                child: Text(
-              state.message,
-              style: TextStyle(color: AppColor.redColor),
-            ));
-          }
-          if (state is WishlistLoaded) {
-            return CustomScrollView(
-              slivers: [
-                SliverList(
-                  delegate: SliverChildBuilderDelegate((context, index) {
-                    final movie = state.movies[index];
+        body: BlocBuilder<WishlistBloc, WishlistState>(
+            bloc: bloc,
+            builder: (context, state) {
+              if (state is WishlistLoading) {
+                return const Center(child: CircularProgressIndicator.adaptive());
+              }
+              if (state is WishlistError) {
+                return Center(
+                    child: Text(
+                  state.message,
+                  style: TextStyle(color: AppColor.redColor),
+                ));
+              }
+              if (state is WishlistLoaded) {
+                if (state.movies.isEmpty) return const Center(child: Text('No movies in wishlist'));
 
-                    return ListTile(
-                      title: Text(movie.title!).bodyLargeBold.white,
-                      subtitle: Text(
-                        movie.overview!,
-                        maxLines: 2,
-                        overflow: TextOverflow.ellipsis,
-                      ).bodyMedium,
-                      trailing: Container(
-                        margin: const EdgeInsets.only(right: 10.0),
-                        width: 120.0,
-                        child: ClipRRect(
-                          borderRadius: BorderRadius.circular(10.0),
-                          child: Image.file(
-                            File(movie.posterPath ?? ''),
-                            fit: BoxFit.cover,
-                            width: 120.0,
-                            height: 160.0,
-                            errorBuilder: (context, error, stackTrace) {
-                              return const Icon(Icons.error);
-                            },
-                          ),
-                        ),
-                      ),
-                    );
-                  }, childCount: state.movies.length),
-                )
-              ],
-            );
-          }
-          return SizedBox();
-        }));
+                return CustomScrollView(
+                  slivers: [
+                    SliverList(
+                      delegate: SliverChildBuilderDelegate((context, index) {
+                        final movie = state.movies[index];
+
+                        return Stack(
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Column(
+                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    children: [
+                                      Text(movie.title ?? '').bodyLargeBold.white,
+                                      Text(movie.releaseDate ?? '').bodyMedium,
+                                    ],
+                                  ),
+                                  ClipRRect(
+                                    borderRadius: BorderRadius.circular(10.0),
+                                    child: Image.file(
+                                      File(movie.posterPath ?? ''),
+                                      fit: BoxFit.cover,
+                                      width: 120.0,
+                                      height: 160.0,
+                                      errorBuilder: (context, error, stackTrace) {
+                                        return const Icon(Icons.error);
+                                      },
+                                    ),
+                                  )
+                                ],
+                              ),
+                            ),
+                            Positioned(
+                                left: 5.0,
+                                top: 0,
+                                child: IconButton.filled(
+                                    style: IconButton.styleFrom(backgroundColor: AppColor.grey),
+                                    onPressed: () {},
+                                    icon:
+                                        Icon(FluentIcons.delete_12_filled, color: AppColor.black)))
+                          ],
+                        );
+                      }, childCount: state.movies.length),
+                    )
+                  ],
+                );
+              }
+              return SizedBox();
+            }));
   }
 }
